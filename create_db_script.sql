8657afc149d416b7d6ae7006ad1d0ba8112c16aa
Caner Demirci <canerdemirci.mail@gmail.com>
2025-08-24 00:37:20 +0300

    add request_body field.

--- create_db_script.sql
+++ create_db_script.sql
@@ -1,10 +1,13 @@
-CREATE TABLE IF NOT EXISTS mock_responses (
+CREATE TABLE IF NOT EXISTS public.mock_responses (
     id SERIAL PRIMARY KEY,
     path VARCHAR(500) NOT NULL,
     method VARCHAR(10) NOT NULL,
-    response_body TEXT,
+    request_body JSONB,
+    response_body JSONB NOT NULL,
     response_status_code INTEGER DEFAULT 200,
     headers TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(path, method)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX IF NOT EXISTS idx_mock_responses_lookup
+ON public.mock_responses (path, method, (md5(request_body::jsonb::text)));
